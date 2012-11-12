8fda387acc3b92361d7abde0589a17093404e7cd
huangyifeng <luckyJerry85@gmail.com>
2012-11-12 15:24:06 +0800

    Initial Commit

---
+++ DoACrash/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DoACrash
+//
+//  Created by HuangYiFeng on 11/12/12.
+//  Copyright (c) 2012 aimob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ DoACrash/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DoACrash
+//
+//  Created by HuangYiFeng on 11/12/12.
+//  Copyright (c) 2012 aimob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
